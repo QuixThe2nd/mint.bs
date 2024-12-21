e2c5cbe4da5040172ba3d303423c76675a058001
Parsa Yazdani (Quix) <parsa@yazdani.au>
2024-12-21 22:17:05 +1100

    Connect

---
+++ src/main.js
@@ -0,0 +1,12 @@
+document.getElementById("connect").addEventListener("click", async () => {
+  if (typeof window.kasware !== 'undefined') {
+    alert('KasWare Wallet is installed!');
+  } else {
+    try {
+      let accounts = await window.kasware.requestAccounts();
+      console.log('connect success', accounts);
+    } catch (e) {
+      console.log('connect failed');
+    }
+  }
+})
